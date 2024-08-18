15acf133a3bbda2f5047a7177e6d659b43244fbc
ridermarra <86563297+ridermarra@users.noreply.github.com>
2024-08-18 12:08:46 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MINER_PRIVATE_KEY: 'YOUR_MINER_PRIVATE_KEY_HERE', // The private key of the wallet that owns the mining pool contract
+    MINING_POOL_ADDRESS: 'YOUR_MINING_POOL_CONTRACT_ADDRESS_HERE',
+    MINE_TX_PER_MINUTE: 50,
+    RPC: 'https://rpc.ankr.com/blast'
+};
