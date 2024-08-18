db417c3e1d7c7e5cf9ec0eb93ed5d37d82ed326a
ridermarra <ridermarangoni@gmail.com>
2024-08-18 14:40:36 +0200

    miningpoolabi + env private key

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
+require('dotenv').config({ path: './deploy/.env' });
+
 module.exports = {
-    MINER_PRIVATE_KEY: 'YOUR_MINER_PRIVATE_KEY_HERE', // The private key of the wallet that owns the mining pool contract
+    MINER_PRIVATE_KEY: process.env.PRIVATE_KEY, // Load the private key from the environment variable
     MINING_POOL_ADDRESS: 'YOUR_MINING_POOL_CONTRACT_ADDRESS_HERE',
     MINE_TX_PER_MINUTE: 50,
     RPC: 'https://rpc.ankr.com/blast'
